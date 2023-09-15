6e26b1c4edcc61e62e9ce1d358d8f53bad7a6fe9
Leon Kaucher <leon.kaucher@threedy.io>
2023-09-15 08:49:59 +0200

    fix: Correct app component test

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -16,9 +16,9 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'webvis-angular-typescript'`, () => {
+  it(`should have as title 'webvis-angular'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('webvis-angular-typescript');
+    expect(app.title).toEqual('webvis-angular');
   });
 });
